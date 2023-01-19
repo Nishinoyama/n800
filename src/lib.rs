09f93277b5a05ca54c8302781ffa8c68d65e7852
Nishinoyama <begoingto1414@gmail.com>
2023-01-19 18:59:25 +0900

    add: memory

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,37 @@ pub mod bits;
 pub mod register;
 
 pub mod alu;
+
+pub mod memory {
+    pub trait Memory {
+        type Data;
+        type Address;
+        fn write(&self, address: Self::Address, data: Self::Data);
+        fn read(&self, address: Self::Address) -> Self::Data;
+    }
+
+    mod gb {
+        struct GBMemory {
+            /// 0x0000-0x3FFF
+            cartridge_rom_00: [u8; 32768],
+            /// 0x4000-0x7FFF
+            cartridge_rom_01: [u8; 32768],
+            /// 0x8000-0x9FFF
+            video_ram: [u8; 8192],
+            /// 0xA000-0xBFFF
+            cartridge_ram: [u8; 8192],
+            /// 0xC000-0xCFFF
+            work_ram_0: [u8; 4096],
+            /// 0xD000-0xDFFF
+            work_ram_1: [u8; 4096],
+            /// 0xFE00-0xFE9F
+            sprite_attribute_table: [u8; 256],
+            /// 0xFF00-0xFF7F
+            io_ports: [u8; 128],
+            /// 0XFF80-0XFFFE
+            high_rom: [u8; 127],
+            /// 0xFFFF
+            interrupt_enable_register: u8,
+        }
+    }
+}

f034ca81e22eab00e40216aa5b044a84381c0600
Nishinoyama <begoingto1414@gmail.com>
2023-01-19 18:41:39 +0900

    add: alu

--- src/alu.rs
+++ src/alu.rs
@@ -1,6 +1,12 @@
 use crate::bits::BitwiseOps;
 use strum::IntoEnumIterator;
 
+pub trait ALU {
+    type Data;
+    /// flag scrambler is containing!
+    fn op(self, lhs: Self::Data, rhs: Self::Data) -> (Self::Data, Self::Data);
+}
+
 pub trait FlagSet<F: ALUFlag>: Sized + BitwiseOps {
     fn set(self, flag: F) -> Self;
     fn reset(self, flag: F) -> Self;
@@ -104,7 +110,7 @@ pub mod bit8 {
 #[cfg(test)]
 mod tests {
     use crate::alu::bit8::FlagSetU8;
-    use crate::alu::{ALUFlag, FlagSet};
+    use crate::alu::{ALUFlag, FlagSet, ALU};
     use strum::IntoEnumIterator;
     use strum_macros::EnumIter;
 
@@ -125,6 +131,61 @@ mod tests {
 
     impl ALUFlag for TestFlag {}
 
+    #[derive(Debug, Default, Copy, Clone, Eq, PartialEq)]
+    struct TestAdder {
+        neg: bool,
+        cin: bool,
+    }
+
+    impl TestAdder {
+        fn adder() -> Self {
+            Self::default()
+        }
+        fn subber() -> Self {
+            Self {
+                neg: true,
+                cin: true,
+            }
+        }
+        fn carried_adder() -> Self {
+            Self {
+                neg: false,
+                cin: true,
+            }
+        }
+        fn borrowed_subber() -> Self {
+            Self {
+                neg: true,
+                cin: false,
+            }
+        }
+    }
+
+    impl ALU for TestAdder {
+        type Data = u8;
+
+        fn op(self, lhs: Self::Data, mut rhs: Self::Data) -> (Self::Data, Self::Data) {
+            if self.neg {
+                rhs = !rhs;
+            }
+            let (res, ovf) = if self.cin {
+                let (res, ovf0) = rhs.overflowing_add(lhs);
+                let (res, ovf1) = res.overflowing_add(1);
+                (res, ovf0 | ovf1)
+            } else {
+                rhs.overflowing_add(lhs)
+            };
+            let mut status = 2;
+            if self.neg ^ ovf {
+                status += TestFlag::Ovf.into_u8();
+            };
+            if res == 0 {
+                status += TestFlag::Zero.into_u8();
+            };
+            (res, status)
+        }
+    }
+
     fn scramble(flags: FlagSetU8<TestFlag>) -> u8 {
         flags
             .into_flags()
@@ -143,5 +204,18 @@ mod tests {
         assert_eq!(scramble(fs | ft), 11);
         assert_eq!(scramble(fs & ft), 10);
         assert_eq!(scramble(!fs & ft), 2);
+    }
+
+    #[test]
+    fn alu() {
+        assert_eq!(TestAdder::adder().op(10, 3), (13, 2));
+        assert_eq!(TestAdder::adder().op(103, 191), (38, 10));
+        assert_eq!(TestAdder::adder().op(1, 255), (0, 11));
+        assert_eq!(TestAdder::carried_adder().op(0, 255), (0, 11));
+        assert_eq!(TestAdder::carried_adder().op(6, 9), (16, 2));
+        assert_eq!(TestAdder::subber().op(10, 3), (7, 2));
+        assert_eq!(TestAdder::subber().op(10, 13), (253, 10));
+        assert_eq!(TestAdder::subber().op(13, 13), (0, 3));
+        assert_eq!(TestAdder::borrowed_subber().op(10, 3), (6, 2));
     }
 }

f66248451c846f3f52d6ed98b9ee07774b27f85c
Nishinoyama <begoingto1414@gmail.com>
2023-01-26 10:38:07 +0900

    add: implement misc status flags

--- src/alu.rs
+++ src/alu.rs
@@ -3,7 +3,7 @@ use enumset::{EnumSet, EnumSetType};
 pub trait ALU {
     type Flag: Flag;
     type Data;
-    fn op(self, lhs: Self::Data, rhs: Self::Data) -> (Self::Data, EnumSet<Self::Flag>);
+    fn op(&self, lhs: Self::Data, rhs: Self::Data) -> (Self::Data, EnumSet<Self::Flag>);
 }
 
 pub trait Flag: Sized + Copy + EnumSetType {}
@@ -24,7 +24,21 @@ enum StatusFlag {
     AuxiliaryCarry,
 }
 
-impl StatusFlag {}
+impl StatusFlag {
+    pub fn set_by_result(result: u8) -> EnumSet<StatusFlag> {
+        let mut flag_set = EnumSet::new();
+        if result == 0 {
+            flag_set |= Self::Zero;
+        }
+        if result >= 0x80 {
+            flag_set |= Self::Sign;
+        }
+        if result.count_ones() % 2 == 0 {
+            flag_set |= Self::Parity;
+        }
+        flag_set
+    }
+}
 
 impl Flag for StatusFlag {}
 
@@ -32,6 +46,29 @@ impl Flag for StatusFlag {}
 mod tests {
     use crate::alu::{StatusFlag, ALU};
     use enumset::EnumSet;
+
+    fn primary_adder(cin: bool, lhs: u8, rhs: u8) -> (u8, EnumSet<StatusFlag>) {
+        let auxiliary_carry = if cin {
+            lhs % 0x10 + rhs % 0x10 + 1 >= 0x10
+        } else {
+            lhs % 0x10 + rhs % 0x10 >= 0x10
+        };
+        let (rhs, carry_ovf) = if cin {
+            rhs.overflowing_add(1)
+        } else {
+            (rhs, false)
+        };
+        let (res, ovf) = lhs.overflowing_add(rhs);
+        let mut status = StatusFlag::set_by_result(res);
+        if carry_ovf | ovf {
+            status |= StatusFlag::Carry;
+        }
+        if auxiliary_carry {
+            status |= StatusFlag::AuxiliaryCarry;
+        }
+        println!("{:x} + {:x} = {:x} ({:?})", lhs, rhs, res, status);
+        (res, status)
+    }
 
     #[derive(Debug, Default, Copy, Clone, Eq, PartialEq)]
     struct TestAdder {
@@ -67,42 +104,63 @@ mod tests {
         type Flag = StatusFlag;
         type Data = u8;
 
-        fn op(self, lhs: Self::Data, mut rhs: Self::Data) -> (Self::Data, EnumSet<Self::Flag>) {
+        fn op(&self, lhs: Self::Data, mut rhs: Self::Data) -> (Self::Data, EnumSet<Self::Flag>) {
             if self.neg {
                 rhs = !rhs;
             }
-            let (res, ovf) = if self.cin {
-                let (res, ovf0) = rhs.overflowing_add(lhs);
-                let (res, ovf1) = res.overflowing_add(1);
-                (res, ovf0 | ovf1)
-            } else {
-                rhs.overflowing_add(lhs)
-            };
-            let mut status = EnumSet::empty();
-            if self.neg ^ ovf {
-                status |= StatusFlag::Carry;
-            };
-            if res == 0 {
-                status |= StatusFlag::Zero;
-            };
+            let (res, mut status) = primary_adder(self.cin, lhs, rhs);
+            if self.neg {
+                status ^= StatusFlag::Carry;
+                status ^= StatusFlag::AuxiliaryCarry;
+            }
             (res, status)
+        }
+    }
+
+    #[derive(Debug, Default, Copy, Clone, Eq, PartialEq)]
+    struct TestBitwiseAnd {}
+
+    impl ALU for TestBitwiseAnd {
+        type Flag = StatusFlag;
+        type Data = u8;
+
+        fn op(&self, lhs: Self::Data, rhs: Self::Data) -> (Self::Data, EnumSet<Self::Flag>) {
+            (lhs & rhs, StatusFlag::AuxiliaryCarry.into())
         }
     }
 
     #[test]
     fn alu() {
         use StatusFlag::*;
-        assert_eq!(TestAdder::adder().op(10, 3), (13, EnumSet::empty()));
-        assert_eq!(TestAdder::adder().op(103, 191), (38, Carry.into()));
-        assert_eq!(TestAdder::adder().op(1, 255), (0, Carry | Zero));
-        assert_eq!(TestAdder::carried_adder().op(0, 255), (0, Carry | Zero));
-        assert_eq!(TestAdder::carried_adder().op(6, 9), (16, EnumSet::empty()));
-        assert_eq!(TestAdder::subber().op(10, 3), (7, EnumSet::empty()));
-        assert_eq!(TestAdder::subber().op(10, 13), (253, Carry.into()));
-        assert_eq!(TestAdder::subber().op(13, 13), (0, Zero.into()));
+        let adder = TestAdder::adder();
+        assert_eq!(adder.op(10, 3), (13, EnumSet::empty()));
+        assert_eq!(adder.op(103, 191), (38, Carry | AuxiliaryCarry));
         assert_eq!(
-            TestAdder::borrowed_subber().op(10, 3),
-            (6, EnumSet::empty())
+            adder.op(1, 255),
+            (0, Carry | Zero | Parity | AuxiliaryCarry)
+        );
+        assert_eq!(adder.op(1, 254), (255, Parity | Sign));
+        assert_eq!(adder.op(0x19, 0x28), (0x41, AuxiliaryCarry | Parity));
+        let c_adder = TestAdder::carried_adder();
+        assert_eq!(
+            c_adder.op(0, 255),
+            (0, Carry | Zero | Parity | AuxiliaryCarry)
+        );
+        assert_eq!(c_adder.op(6, 9), (16, AuxiliaryCarry.into()));
+        let subber = TestAdder::subber();
+        assert_eq!(subber.op(18, 3), (15, Parity | AuxiliaryCarry));
+        assert_eq!(subber.op(16, 19), (253, Sign | Carry | AuxiliaryCarry));
+        assert_eq!(subber.op(9, 9), (0, Zero | Parity));
+        let b_subber = TestAdder::borrowed_subber();
+        assert_eq!(b_subber.op(10, 3), (6, Parity.into()));
+        let alus: Vec<Box<dyn ALU<Flag = StatusFlag, Data = u8>>> = vec![
+            (Box::new(TestAdder::adder())),
+            (Box::new(TestAdder::subber())),
+            (Box::new(TestBitwiseAnd {})),
+        ];
+        println!(
+            "{:?}",
+            alus.iter().map(|alu| alu.op(31, 41)).collect::<Vec<_>>()
         );
     }
 }
